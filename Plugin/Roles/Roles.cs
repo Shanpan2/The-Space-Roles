48a660c05629a31afbf0097249f4915d6146c767
supeshio <supeshio314@gmail.com>
2024-03-02 13:53:45 +0900

    add basic of roles
    RolesフォルダとRole関連のものを入れた

---
+++ Plugin/Roles/Roles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSpaceRoles
+{
+    public static class Roles
+    {
+        public static void SetRole(byte playerId,byte roleId)
+        {
+            //var p = PlayerControl.AllPlayerControls.ToArray().First(x => x.PlayerId == playerId).PlayerId;
+            
+        }
+    }
+}
---
+++ Plugin/Rpc.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using Hazel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSpaceRoles
+{
+    [HarmonyPatch]
+    public static class Rpc
+    {
+        public enum Rpcs:byte{
+            SetRole=80,
+            ChangeRole,
+            GameEnd,
+            SendSetting,
+            UseAbility,
+        }
+
+
+
+        [HarmonyPatch(typeof(PlayerControl),nameof(PlayerControl.HandleRpc))]
+        public static class Hud
+        {
+            public static void Prefix(PlayerControl __instance) 
+            {
+
+            }
+            public static void Postfix(byte callId, MessageReader reader)
+            {
+                switch(callId) 
+                {
+                    case (byte)Rpcs.SetRole:
+                        break;
+                }
+            }
+        }
+    }
+}
