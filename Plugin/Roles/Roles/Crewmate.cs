a245aa75e728f8f7476dfe3a7f67bee7b3ded39b
supeshio <supeshio314@gmail.com>
2024-03-12 22:55:39 +0900

    Create Impostor and Crewmate RoleMaster
    その名の通り RoleMasterの作成

---
+++ Plugin/Roles/Roles/Crewmate.cs
@@ -0,0 +1,18 @@
+﻿using Hazel;
+using System.Linq;
+using TheSpaceRoles.Plugin.Roles;
+using UnityEngine;
+using static TheSpaceRoles.Helper;
+
+namespace TheSpaceRoles
+{
+    public class Crewmate : RoleMaster
+    {
+        public Crewmate()
+        {
+            teamsSupported = [Teams.Crewmate];
+            Role = Roles.Crewmate;
+            Color = Palette.CrewmateBlue;
+        }
+    }
+}
---
+++ Plugin/Roles/Roles/Impostor.cs
@@ -0,0 +1,18 @@
+﻿using Hazel;
+using System.Linq;
+using TheSpaceRoles.Plugin.Roles;
+using UnityEngine;
+using static TheSpaceRoles.Helper;
+
+namespace TheSpaceRoles
+{
+    public class Impostor : RoleMaster
+    {
+        public Impostor()
+        {
+            teamsSupported = [Teams.Impostor];
+            Role = Roles.Impostor;
+            Color = Palette.ImpostorRed;
+        }
+    }
+}
