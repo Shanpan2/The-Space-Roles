3853a25c821fcb845fff7281b0654cef8301fb82
supeshio <supeshio314@gmail.com>
2024-05-03 20:24:12 +0900

    [update]  RoleOption
    > RoleOption update
    - Add RoleOption Option

---
+++ Plugin/Options/RoleOptions/RoleOptionOptions.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheSpaceRoles
+{
+    public static class RoleOptionOptions
+    {
+        public static void Check(Teams teams, Roles roles)
+        {
+
+            CustomOptionsHolder.RoleOptions.Do(x => x.@object.active = false);
+            int i = 0;
+            foreach (var ro in CustomOptionsHolder.RoleOptions.Where(x => x.role == roles && x.team == teams))
+            {
+                ro.@object.SetActive(true);
+                ro.Check(i + 4);
+                i++;
+            }
+        }
+    }
+}
